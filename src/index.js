73b4e835849b366d96d527780f9ee59b36280bbb
CescoMinchia <149020166+CescoMinchia@users.noreply.github.com>
2024-01-27 10:57:30 +0000

    created module to lay out home page

--- src/index.js
+++ src/index.js
@@ -1,30 +1,3 @@
-import "./style.css";
-import piazzaGalimbertiImg from "./piazza galimberti.jpeg";
+import { layoutHome } from "./initial-page-load";
 
-function homeImg() {
-  const heroImg = document.createElement("img");
-  heroImg.src = piazzaGalimbertiImg;
-
-  return heroImg;
-}
-
-function homeHeadline() {
-  const headline = document.createElement("h1");
-  headline.textContent = "Beautiful Restaurant in Cuneo";
-  return headline;
-}
-
-function homeText() {
-  const text = document.createElement("p");
-  text.textContent = `
-  This new restaurant has a lot of bagna cauda
-  `;
-  return text;
-}
-
-// lay out all the components
-const content = document.getElementById("content");
-
-content.appendChild(homeHeadline());
-content.appendChild(homeImg());
-content.appendChild(homeText());
+layoutHome();
---
+++ src/initial-page-load.js
@@ -0,0 +1,34 @@
+import "./style.css";
+import piazzaGalimbertiImg from "./piazza galimberti.jpeg";
+
+function homeImg() {
+  const heroImg = document.createElement("img");
+  heroImg.src = piazzaGalimbertiImg;
+
+  return heroImg;
+}
+
+function homeHeadline() {
+  const headline = document.createElement("h1");
+  headline.textContent = "Beautiful Restaurant in Cuneo";
+  return headline;
+}
+
+function homeText() {
+  const text = document.createElement("p");
+  text.textContent = `
+  This new restaurant has a lot of bagna cauda
+  `;
+  return text;
+}
+
+function layoutHome() {
+  // lay out all the components
+  const content = document.getElementById("content");
+
+  content.appendChild(homeHeadline());
+  content.appendChild(homeImg());
+  content.appendChild(homeText());
+}
+
+export {layoutHome}
