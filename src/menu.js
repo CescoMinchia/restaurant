8b2f08606285c2309026dd7eac386cf7c5dbbacf
CescoMinchia <149020166+CescoMinchia@users.noreply.github.com>
2024-02-29 21:21:42 +0000

    Menu Done

--- src/menu.js
+++ src/menu.js
@@ -1,57 +1,69 @@
-import "./style.css";
-import piazzaGalimbertiImg from "./images/piazza galimberti.jpeg";
+import "./menu.css";
+import salmonImg from "./images/menu/salmon-desktop-tablet@2x.jpg";
+import beefImg from "./images/menu/beef-desktop-tablet@2x.jpg";
+import mousseImg from "./images/menu/chocolate-desktop-tablet@2x.jpg";
 import { renderPage } from "./helper.js";
 
-// function importAll(r) {
-//   let images = {};
-//   r.keys().map((item, index) => {
-//     images[item.replace("./", "")] = r(item);
-//   });
-//   return images;
-// }
+function createArticle() {
+  const article = document.createElement("article");
+  const div = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
 
-//<img src={images["doggy.png"]} />;
+  article.appendChild(div);
+  div.appendChild(h3);
+  div.appendChild(p);
 
-function menuImg() {
-  const heroImg = document.createElement("img");
-  heroImg.src = piazzaGalimbertiImg;
+  h3.textContent = "A few highlights from our menu";
+  p.textContent = "We cater for all dietary requirements, but here's a glimpse at some of our diner's favourites. Our menu is revamped every season.";
 
-  return heroImg;
+  return article;
 }
 
-function menuHeadline() {
-  const headline = document.createElement("h1");
-  headline.textContent = "Menu";
-  return headline;
+function createCardsContainer() {
+  const cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("cardsContainer");
+
+  const card1 = createCard(salmonImg,
+    "Seared Salmon Fillet",
+    "Our locally sourced salmon served with a refreshing buckwheat summer salad.");
+
+  const card2 = createCard(beefImg,
+    "Rosemary Filet Mignon",
+    "Our prime beef served to your taste with a delicious choice of seasonal sides.");
+
+  const card3 = createCard(mousseImg,
+    "Summer Fruit Chocolate Mousse",
+    "Creamy mousse combined with summer fruits and dark chocolate shavings.");
+
+  cardsContainer.appendChild(card1);
+  cardsContainer.appendChild(card2);
+  cardsContainer.appendChild(card3);
+
+  return cardsContainer;
 }
 
-function carousel() {
-  // import all images from carousel folder
-  // const images = importAll(
-  //   require.context("./images/carousel", false, /\.(png|jpe?g|svg)$/),
-  // );
-  const container = document.createElement("div");
-  container.classList.add("carousel");
+function createCard(imgUrl, hText, pText) {
+  const div = document.createElement("div");
+  div.classList.add("card");
 
   const img = document.createElement("img");
-  img.src = piazzaGalimbertiImg;
+  const h4 = document.createElement("h4");
+  const p = document.createElement("p");
 
-  container.appendChild(img);
-  // img.setAttribute("height", "50px");
-  // img.setAttribute("width", "auto");
-  return container;
-}
+  img.src = imgUrl;
+  div.appendChild(img);
+  div.appendChild(h4);
+  div.appendChild(p);
 
-function menuText() {
-  const text = document.createElement("p");
-  text.textContent = `
-  This new restaurant has a lot of bagna cauda
-  `;
-  return text;
+  h4.textContent = hText;
+  p.textContent = pText;
+
+  return div;
 }
 
 function renderMenu() {
-  renderPage(carousel(), menuText());
+  renderPage(createArticle(),createCardsContainer());
 }
 
 export { renderMenu };
